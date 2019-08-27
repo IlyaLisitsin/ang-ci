861fa6058a73cabb855f155b95483eb574a64937
Illia <ilyas.sporichev@gmail.com>
2019-08-27 13:12:45 +0300

    build fix

--- src/app/shared/services/login/login.service.ts
+++ src/app/shared/services/login/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 import { HttpClient } from '@angular/common/http';
 import { AUTH_APIS } from '../../constants/apis';
 import { of } from 'rxjs/observable/of';
