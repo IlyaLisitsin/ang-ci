e0fcd6ee55548faa094f20c3e28814ce52bec869
Illia <ilyas.sporichev@gmail.com>
2019-09-11 01:13:02 +0300

    api url fix

--- src/app/shared/constants/apis.ts
+++ src/app/shared/constants/apis.ts
@@ -1,5 +1,4 @@
-const BASE_BE_URL = 'http://localhost:5000/api';
-// const BASE_BE_URL = 'https://anc-ci-be.herokuapp.com/api';
+const BASE_BE_URL = 'https://anc-ci-be.herokuapp.com/api';
 
 export const AUTH_APIS: any = {
   login: `${BASE_BE_URL}/users/login`,
