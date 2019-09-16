03f06791c947c9f68dff4bca4d0c7f01e138161b
Illia <ilyas.sporichev@gmail.com>
2019-09-16 18:17:58 +0300

    build fix

--- src/app/home/components/steps/upload-picture/upload-picture.component.ts
+++ src/app/home/components/steps/upload-picture/upload-picture.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { WebcamImage, WebcamInitError } from 'ngx-webcam';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 import { FormGroup } from '@angular/forms';
 import { Subject } from 'rxjs/Subject';
 
